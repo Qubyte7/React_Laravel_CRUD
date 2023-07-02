a467a46b83c6c2f02bd845be9343fbd9ba506864
Shamiinnocent <122034561+Shamiinnocent@users.noreply.github.com>
2023-07-02 12:25:58 +0200

    done creating ui adding product

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,6 +3,20 @@ import  React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './components/App'
 import { BrowserRouter } from 'react-router-dom';
+
+//sweetalert2
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import 'sweetalert2/dist/sweetalert2.css'
+window.swal = Swal
+const toast =Swal.mixin({
+    toast:true,
+    position: 'top-end',
+    showConfirmButton:false,
+    timer: 3000,
+    timeProgressBar: true,
+})
+window.toast = toast
+
 ReactDOM.createRoot(document.getElementById('app')).render(
     <BrowserRouter >
     <App />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel CRUD with React</title>
-        @viteReactRefresh
+        <!-- @viteReactRefresh -->
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     </head>
     <body>
